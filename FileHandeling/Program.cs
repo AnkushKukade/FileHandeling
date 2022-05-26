c5fa8ffd85ce619b73fd354aca22f88414c37a16
AnkushKukade <ankushkukade1@gmail.com>
2022-05-26 23:05:36 +0530

    Add project files.

---
+++ FileHandeling/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace FileHandeling
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            WriteReadFile wf = new WriteReadFile();
+            //  wf.WriteFile();
+            wf.ReadFile();
+            Console.Read();
+
+
+
+        }
+    }
+}
---
+++ FileHandeling/WriteReadFile.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileHandeling
+{
+    public class WriteReadFile
+    {
+        public void WriteFile()
+        {
+            try
+            {
+                 // provide ---> \\ or @ , to path to remove errors in path.
+                FileStream fs = new FileStream(@"D:\IKS\dotnet\file.txt", FileMode.Create, FileAccess.Write);
+                StreamWriter sw = new StreamWriter(fs);
+
+                Console.WriteLine("Students details are as follow : ");
+                Console.WriteLine("Enter Name : ");
+                String Name = Console.ReadLine();
+                sw.WriteLine(Name);
+
+                Console.WriteLine("Enter ID : ");
+                int id = Convert.ToInt32(Console.ReadLine());
+                sw.WriteLine(id);
+
+                Console.WriteLine("Enter College Name : ");
+                string clgname = Console.ReadLine();
+                sw.WriteLine(clgname);
+
+                sw.Close();
+                fs.Close();
+                Console.WriteLine("File Created...");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public void ReadFile()
+        {
+            FileStream fs = new FileStream(@"D:\IKS\dotnet\file.txt", FileMode.Open, FileAccess.Read);
+           // StreamWriter sw = new StreamWriter(fs);
+            StreamReader sr = new StreamReader(fs);
+
+            while(sr.Peek()> 0)
+            {
+                Console.WriteLine(sr.ReadLine());
+            }
+
+        }
+    }
+}
